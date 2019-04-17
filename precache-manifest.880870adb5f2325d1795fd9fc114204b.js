dd8878a39a6c1b45afed5dac0954f39b167c9c06
Natcancein <nataliaconstanz@gmail.com>
2019-04-17 12:51:38 -0400

    Updates

---
+++ precache-manifest.880870adb5f2325d1795fd9fc114204b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fefced6fd426e504746",
+    "url": "/random-quote-machine/static/js/runtime~main.296aa1cf.js"
+  },
+  {
+    "revision": "17db768b81f88b2cbd02",
+    "url": "/random-quote-machine/static/js/main.c7c17732.chunk.js"
+  },
+  {
+    "revision": "4dee19254916a420092f",
+    "url": "/random-quote-machine/static/js/2.b655f2c0.chunk.js"
+  },
+  {
+    "revision": "17db768b81f88b2cbd02",
+    "url": "/random-quote-machine/static/css/main.453c79ea.chunk.css"
+  },
+  {
+    "revision": "73bff4372e35f721ba3ed32f22dde74f",
+    "url": "/random-quote-machine/index.html"
+  }
+];
